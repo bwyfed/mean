d7abd246f4321a5130a636bfcfec289fc73aa456
Bianwy <bianwy025@qq.com>
2021-03-09 18:11:42 +0800

    创建User模式与模型；注册User模型。

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,6 +2,10 @@ const config = require("./config"),
   mongoose = require("mongoose");
 
 module.exports = function () {
-  const db = mongoose.connect(config.db);
+  const db = mongoose.connect(config.db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  require("../app/models/user.server.model");
   return db;
 };
